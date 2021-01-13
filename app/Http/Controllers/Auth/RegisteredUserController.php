bbd3ce484aa7d5a9570c2fea581e0856b58618e8
Jepisa <javierjeanpieres@gmail.com>
2021-01-13 16:57:56 -0300

    fix de escritura. En vez de "rules" puse "rule"

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -77,7 +77,7 @@ class RegisteredUserController extends Controller
             }
             if( Auth::user()->role->name == 'Programador' )
             {
-                $ruleRole_id = 'exists:rule,id';
+                $ruleRole_id = 'exists:rules,id';
             }
         }
 
