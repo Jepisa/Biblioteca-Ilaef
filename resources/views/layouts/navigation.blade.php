4d2f78d9e8e8c6a13d87973cbbceb208f7bd40c6
Jepisa <javierjeanpieres@gmail.com>
2021-03-18 04:13:06 -0300

    ajustes para la prueba anterior

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,6 +1,6 @@
 <nav x-data="{ open: false }" class="">
     <!-- Primary Navigation Menu -->
-    <div class="w-full mx-1 px-4 sm:px-6 lg:px-8 h-full">
+    <div class="w-full h-full">
         <div class="flex justify-between h-full">
             <div class="flex">
                 <!-- Logo -->
@@ -10,17 +10,17 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                <div class="hidden links space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
                         {{ __('Inicio') }}
                     </x-nav-link>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                <div class="hidden links space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('aboutUs')" :active="request()->routeIs('aboutUs')">
                         {{ __('Nosotros') }}
                     </x-nav-link>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                <div class="hidden links space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('contact')" :active="request()->routeIs('contact')">
                         {{ __('Contacto') }}
                     </x-nav-link>
@@ -32,7 +32,12 @@
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                            <div>{{ Auth::user()->name }}</div>
+                            @auth
+                                <div>{{ Auth::user()->name }}</div>
+                            @endauth
+                            @guest
+                                Hola!
+                            @endguest
 
                             <div class="ml-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
@@ -44,19 +49,27 @@
 
                     <x-slot name="content">
                         <!-- Authentication -->
-                        <form method="POST" action="{{ route('logout') }}">
+                        @auth
+                            <form method="POST" action="{{ route('logout') }}">
                             @csrf
 
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Logout') }}
-                            </x-dropdown-link>
-                        </form>
-                        <x-dropdown-link :href="route('register')">
+                                <x-dropdown-link :href="route('logout')"
+                                        onclick="event.preventDefault();
+                                                    this.closest('form').submit();">
+                                    {{ __('Logout') }}
+                                </x-dropdown-link>
+                            </form>
+                        @endauth
+                        @guest
+                            <x-dropdown-link :href="route('register')">
                                 {{ __('Register') }}
-                        </x-dropdown-link>
+                            </x-dropdown-link>
+                        @endguest
+                        
+@auth
+    
 
+@programmer
                         @foreach (App\Models\User::all() as $user)
                             
                             <form method="POST" action="{{ route('impersonate.start', [$user->id]) }}" class="{{ (Auth::user()->email == $user->email) ? 'border font-black': ''}}">
@@ -69,7 +82,9 @@
                                 </x-dropdown-link>
                             </form>
                         @endforeach
+@endprogrammer
 
+@endauth
                     </x-slot>
                 </x-dropdown>
             </div>
@@ -103,10 +118,6 @@
                     </svg>
                 </div>
 
-                <div class="ml-3">
-                    <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                    <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-                </div>
             </div>
 
             <div class="mt-3 space-y-1">
