1babf78472fe600b2a09a6227e47ca77ec7add75
jepisa <javierjeapieres@gmail.com>
2022-02-23 20:34:24 -0300

    add - propiedad "type" a los más visitados

--- app/Models/Counter.php
+++ app/Models/Counter.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Counter extends Model
 {
@@ -18,11 +19,21 @@ class Counter extends Model
 
     public static function mostRelevants($limit)
     {
-        $relevants = Counter::orderBy('views')->limit($limit)->get();
+        $relevants = Counter::orderBy('views', 'desc')->limit($limit)->get();
         $result = collect();
         foreach ($relevants as $relevant) {
-            $result =  $result->push($relevant->countable);
+            $content = $relevant->countable;
+            $modelName = get_class($content);
+
+            $content->type = self::nameModelType($modelName);
+            $result =  $result->push($content);
         }
         return  $result->unique();
+    }
+
+    private static function nameModelType($modelName)
+    {
+        $nameModel = Str::slug($modelName);
+        return $nameModel;
     }
 }
